ac7507493eca445a951842f63cf7913232eef90d
francisca hernandez <francisca.hernandez17@gmail.com>
2022-11-15 19:59:43 -0800

    corrected connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,12 @@
-require('dotenv').config();
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
 
-let sequelize;
-
-
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL)
-} else {
-    sequelize = new Sequelize(
-        process.env.MYSQL_DB,
-        process.env.MYSQL_USER,
-        process.env.MYSQL_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306,
-        }
-    );
-}
-
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
 
 module.exports = sequelize;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const apiRoutes = require('.api');
+const apiRoutes = require('./api');
 
 
-router.use('/api', apiRoutes);
+router.use('./api', apiRoutes);
 // router.use('/categories', categoryRoutes);
 // router.use('/products', productRoutes);
 // router.use('/tags', tagRoutes);
+
+router.use((req, res) => {
+    res.send
+  });
 
 module.exports = router;
